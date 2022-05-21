c045a7d72aa0357909ea805db7c6771595a11df8
Suheidy19 <suhe_nerina999@hotmail.com>
2022-05-21 11:11:59 -0500

    actualización de Suhe

--- extractor/views.py
+++ extractor/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import TemplateView
+
+
+class IndexView(TemplateView):
+    # model = Cultivos
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['title'] = 'Extractor de noticias'
+        return context
