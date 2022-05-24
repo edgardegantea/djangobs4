4385c1972cdd12e76d65159d128225fed2b619f0
Suheidy19 <suhe_nerina999@hotmail.com>
2022-05-23 23:02:03 -0500

    actualización de Suhe

--- extractor/views.py
+++ extractor/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Contacto
+from bs4 import BeautifulSoup
+import requests
 
 # Create your views here.
 from django.views.generic import TemplateView
@@ -9,7 +11,15 @@ class IndexView(TemplateView):
     model = Contacto
     template_name = 'index.html'
 
+    def extraer(self):
+        url = "https://www.tutorialspoint.com/index.htm"
+        req = requests.get(url)
+        soup = BeautifulSoup(req.text, "html.parser")
+        print(soup.title)
+
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         context['title'] = 'Extractor de noticias'
+        context['req'] = requests.get("https://www.tutorialspoint.com/index.htm")
+
         return context
--- manage.py
+++ manage.py
@@ -3,6 +3,29 @@
 import os
 import sys
 
+#!CODIGO NUEVO DE BEAUTIFUL SOUP
+import requests
+from bs4 import BeautifulSoup
+
+url= "https://www.elsoldemexico.com.mx/metropoli/cdmx/mueren-tres-personas-por-balacera-en-colonia-la-roma-8324832.html"
+
+headers ={
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36 OPR/86.0.4363.59",
+    "Agent-Languaje": "es",
+}
+r=requests.get(url,headers=headers)
+
+soup = BeautifulSoup(r.text,"lxml")
+#print(soup.prettify())
+
+name = soup.select_one(selector=".title").getText()
+name=name.strip()
+print(name)
+
+contenido=soup.select_one(selector="#content-body-225-8324832").getText()
+contenido=contenido.strip()
+print(contenido)
+
 
 def main():
     """Run administrative tasks."""
