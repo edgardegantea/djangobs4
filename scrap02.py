600844077acdc0e048ea25a8ce087b7ea83229fe
Suheidy19 <suhe_nerina999@hotmail.com>
2022-06-16 20:51:09 -0500

    extracion de noticias

---
+++ scrap02.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+
+# extraer datos de una pagina
+
+website = 'https://www.elsoldemexico.com.mx'
+result = requests.get(website)
+content = result.text
+soup = BeautifulSoup(content, 'lxml')
+#print(soup.prettify())
+
+
+#noticia 01
+box = soup.find('h4', class_='tit-top-story')
+title = box.find('a').get_text()
+print(title)
+box = soup.find('section', class_='mexico_justicia')
+transcript = box.find('p', class_='extract').get_text(strip=True,separator=' ')
+print(transcript)
+
+
+#noticia 02
+box = soup.find('h4', class_='title')
+title = box.find('a').get_text()
+print(title)
+box = soup.find('article', class_='col-sm-5 hard-news-1')
+transcript = box.find('p', class_='summary').get_text(strip=True,separator=' ')
+print(transcript)
+
+
+
+
+
+
