6d3d26abffc4bc0596e57fa7a717ee3dae039cf7
Suheidy19 <suhe_nerina999@hotmail.com>
2022-06-20 19:36:09 -0500

    modificacion de scraps

--- scrap01.py
+++ scrap01.py
@@ -1,30 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 
-#extraer datos de una pagina
+# extraer datos de una pagina
 website = 'https://elpais.com/mexico/'
 result = requests.get(website)
 content = result.text
 soup = BeautifulSoup(content, 'lxml')
-#print(soup.prettify())
 
+# Entrar a seccion que contiene bloques
+layout = soup.find('div', class_="b-f_w b_row-md _g _g-md")
+print(layout)
 
+# Entrar a seccion que contiene articulos y mostrar su cantidad
+blocks = layout.find_all(class_='c c-d c--m-n', recursive=True)
+print(len(blocks))
 
-#noticia 01
-box = soup.find('section', class_='_g _g-md _g-o b b-d b--o')
-title = box.find('a').get_text()
-print(title)
-#scrip borra espacios en blanco al inicio y al final de una cadena de texto
-transcript = box.find('p', class_='c_d').get_text(strip=True,separator=' ')
-print(transcript)
+for block in blocks:
+    for href in block.find('a', href=True):
+        print('Titulo: ' + href.get_text())
 
+        Noticia = block.find('p', class_='c_d').get_text()
+        print('Noticia: ' + Noticia)
 
-
-#noticia 02
-
-box = soup.find('div', class_='b-d_b b_op _g _g-md b_op-1-2')
-title = box.find('h2', class_='c_t').get_text(strip=False, separator=' ')
-print(title)
-transcript = box.find('p', class_='c_d').get_text(strip=False, separator=' ')
-print(transcript)
-
+        print()
--- scrap04.py
+++ scrap04.py
@@ -3,25 +3,22 @@ import requests
 from bs4 import BeautifulSoup
 
 # extraer datos de una pagina
-website = 'https://elpais.com/mexico/'
+website = 'https://www.elfinanciero.com.mx'
 result = requests.get(website)
 content = result.text
 soup = BeautifulSoup(content, 'lxml')
 
 # Entrar a seccion que contiene bloques
-layout = soup.find('div', class_="b-f_w b_row-md _g _g-md")
+layout = soup.find('div', class_="row advanced-grid")
 print(layout)
 # Entrar a seccion que contiene articulos y mostrar su cantidad
-blocks = layout.find_all(class_='c c-d c--m-n', recursive=True)
+blocks = layout.find_all(class_='container-fluid medium-promo', recursive=True)
 print(len(blocks))
 
 for block in blocks:
     for href in block.find_all('a', href=True):
         print('Titulo: ' + href.get_text())
 
-        Noticia = block.find('p', class_='c_d').get_text()
+        Noticia = block.find('p', class_='secondary-font__SecondaryFontStyles-x1tol1-0 dhTDff description-text').get_text()
         print('Noticia: ' + Noticia)
-
-        #Autor = block.find('div', class_='c_a').get_text()
-        #print('Autor: ' + Autor)
         print()
